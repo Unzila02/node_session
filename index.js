1b4dce306bef63278e3c8f1ee0e57c4767a09a6f
Unzila02 <unzilamaqsood123@gmail.com>
2022-10-15 02:19:53 -0700

    first version

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-console.log("welcome to node js session with me :)))");
-alert("assalamualaikum everyone welcome to my world");
-prompt("enter user name");
+const fs = require("fs");
+
+fs.writeFileSync("read.txt", "welcome to my first code ");
+// fs.writeFileSync("read.txt", "unzila, welcome to ");
+fs.appendFileSync("read.txt" , "  i write my next code.i am very happy");
+const buf_data =fs.readFileSync("read.txt");
+console.log(buf_data);
